7c9b55b754085db70603332656bdcc6c5dc42549
Le Thanh Nam <lethanhnam330@gmail.com>
2024-09-28 17:35:26 +0700

    completed check email and password

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,8 +1,33 @@
 const User = require('../models/User');
 
 const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+var JWTAction = require('../middlewares/JWT.action');
+
+let refreshTokenArray = [];
+
+const generateAccessToken = async (user) => {
+    const payload = {
+        _id: user._id,
+        email: user.email,
+        isAdmin: user.isAdmin,
+    };
+
+    const access_token = await JWTAction.createJWT(payload, process.env.ACCESS_KEY, '7d').catch((err) => null);
+
+    return access_token;
+};
+
+const generateRefreshToken = async (user) => {
+    const payload = { _id: user._id, email: user.email };
+
+    const refresh_token = await JWTAction.createJWT(payload, process.env.REFRESH_KEY, '8d').catch((err) => null);
+
+    return refresh_token;
+};
 
 class AuthController {
+    //POST /auth/register
     async registerUser(req, res, next) {
         try {
             const { name, email, password, profile_pic } = req.body;
@@ -11,9 +36,9 @@ class AuthController {
 
             if (checkEmail) {
                 return next({
-                    statusCode: 400,
+                    statusCode: 401,
                     message: `Email ${req.body.email} is exists. Please register another email.`,
-                    error: 'Bad request',
+                    error: 'Unauthorized',
                 });
             }
 
@@ -39,6 +64,150 @@ class AuthController {
                 error: 'Server error',
             });
         }
+    }
+
+    //POST /auth/login
+    async login(req, res, next) {
+        const user = await User.findOne({ email: req.body.email })
+            .then((response) => {
+                if (!response)
+                    return res.status(401).json({
+                        statusCode: 401,
+                        message: 'Email not correct!',
+                        error: 'Unauthorized',
+                    });
+                return response;
+            })
+            .catch(() => null);
+
+        if (!user) {
+            return next({
+                statusCode: 400,
+                message: 'Login email failed',
+                error: 'Bad request',
+            });
+        }
+
+        //Check password
+        bcrypt
+            .compare(req.body.password, user.password)
+            .then(async (result) => {
+                if (!result)
+                    return res.status(401).json({
+                        statusCode: 401,
+                        message: 'Password not correct!',
+                        error: 'Unauthorized',
+                    });
+
+                const access_token = await generateAccessToken(user);
+                const refresh_token = await generateRefreshToken(user);
+
+                console.log('access_token', access_token);
+
+                if (!access_token | !refresh_token) {
+                    return next({
+                        statusCode: 500,
+                        message: 'Create token failed',
+                        error: 'Unauthorized',
+                    });
+                }
+
+                refreshTokenArray.push(refresh_token);
+
+                //Save refresh_token into cookie...
+                res.cookie('refresh_token', refresh_token, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: 'none',
+                    path: '/',
+                    maxAge: 60 * 60 * 1000,
+                });
+
+                const { password, ...payloads } = user._doc;
+                return res.status(201).json({
+                    statusCode: 201,
+                    message: 'Login successfully',
+                    data: { access_token, user: payloads },
+                });
+            })
+            .catch(() =>
+                next({
+                    statusCode: 400,
+                    message: 'Login password failed',
+                    error: 'Bad request',
+                }),
+            );
+    }
+
+    //POST /auth/email
+    async checkEmail(req, res, next) {
+        const user = await User.findOne({ email: req.body.email })
+            .select('-password')
+            .catch(() => null);
+
+        if (!user) {
+            return next({
+                statusCode: 401,
+                message: 'Email not correct!',
+                error: 'Unauthorized',
+            });
+        }
+
+        return res.status(200).json({
+            statusCode: 200,
+            message: 'Email verify',
+            data: user,
+        });
+    }
+
+    //POST /auth/password
+    async checkPassword(req, res, next) {
+        const user = await User.findById(req.body.userId).catch(() => null);
+
+        if (!user) {
+            return next({
+                statusCode: 400,
+                message: 'Login email failed',
+                error: 'Unauthorized',
+            });
+        }
+
+        //Check password
+        await bcrypt
+            .compare(req.body.password, user.password)
+            .then(async (result) => {
+                if (!result)
+                    return res.status(401).json({
+                        statusCode: 401,
+                        message: 'Password not correct!',
+                        error: 'Unauthorized',
+                    });
+
+                const tokenData = { id: user._id, email: user.email };
+                const token = await jwt.sign(tokenData, process.env.ACCESS_KEY, { expiresIn: '1d' });
+
+                //Save refresh_token into cookie...
+                res.cookie('token', token, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: 'none',
+                    path: '/',
+                    maxAge: 60 * 60 * 1000,
+                });
+
+                return res.status(200).json({
+                    statusCode: 200,
+                    message: 'Login successfully',
+                    token: token,
+                });
+            })
+            .catch(() =>
+                next({
+                    statusCode: 400,
+                    message: 'Login password failed',
+                    error: 'Bad request',
+                }),
+            );
     }
 }
 
--- app/routes/authRoute.js
+++ app/routes/authRoute.js
@@ -5,6 +5,12 @@ const AuthController = require('../controllers/AuthController');
 
 router.post('/register', AuthController.registerUser);
 
+router.post('/login', AuthController.login);
+
+router.post('/email', AuthController.checkEmail);
+
+router.post('/password', AuthController.checkPassword);
+
 router.use((err, req, res, next) => {
     const statusCode = err.statusCode ?? 500;
     res.status(statusCode).json({ service: 'Auth_Api', ...err });
