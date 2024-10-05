badfeb791b66c45d0e809187551216fb848aa755
Le Thanh Nam <lethanhnam330@gmail.com>
2024-10-05 09:04:22 +0700

    upload file interface

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -8,12 +8,22 @@ export default function RegisterPage() {
         profile_pic: '',
     });
 
+    const [uploadPhoto, setUploadPhoto] = useState<File | null>(null);
+
     const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
 
         setData((prev) => {
             return { ...prev, [name]: value };
         });
+    };
+
+    const handleUploadPhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files && e.target.files[0];
+
+        if (file) {
+            setUploadPhoto(file);
+        }
     };
 
     return (
@@ -23,7 +33,7 @@ export default function RegisterPage() {
                     Welcome to Chat app!
                 </h5>
 
-                <form>
+                <form className="d-grid gap-1 mt-2">
                     <div className="d-flex flex-column gap-1">
                         <label htmlFor="name" className=" col-form-label">
                             Name:
@@ -40,6 +50,63 @@ export default function RegisterPage() {
                                 required
                             />
                         </div>
+                    </div>
+
+                    <div className="d-flex flex-column gap-1">
+                        <label htmlFor="email" className=" col-form-label">
+                            Email:
+                        </label>
+                        <div className="">
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                placeholder="Enter your email"
+                                className="form-control"
+                                value={data.email}
+                                onChange={handleOnChange}
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <div className="d-flex flex-column gap-1">
+                        <label htmlFor="password" className=" col-form-label">
+                            Password:
+                        </label>
+                        <div className="">
+                            <input
+                                type="text"
+                                id="password"
+                                name="password"
+                                placeholder="Enter your password"
+                                className="form-control"
+                                value={data.password}
+                                onChange={handleOnChange}
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <div className="d-flex flex-column gap-1">
+                        <label htmlFor="profile_pic" className=" col-form-label">
+                            Photo:
+                            <div
+                                className="d-flex justify-content-center align-items-center rounded "
+                                style={{ backgroundColor: '#E2E8F0', height: '100%', cursor: 'pointer' }}
+                            >
+                                <p className="m-0">{uploadPhoto ? uploadPhoto.name : ' Upload profile photo'}</p>
+                            </div>
+                        </label>
+                        <input
+                            type="file"
+                            id="profile_pic"
+                            name="profile_pic"
+                            className="form-control visually-hidden"
+                            value={data.profile_pic}
+                            onChange={handleUploadPhoto}
+                            required
+                        />
                     </div>
                 </form>
             </div>
