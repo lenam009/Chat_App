e96e0b597748e9dcbeb23eb852c5971170f4d3a0
Le Thanh Nam <lethanhnam330@gmail.com>
2025-06-30 09:17:05 +0700

    update

--- app/socket/index.js
+++ app/socket/index.js
@@ -164,7 +164,7 @@ io.on('connection', async (socket) => {
 
     /** Disconnect */
     socket.on('disconnect', () => {
-        if (onlineUser.size !== 0) onlineUser.delete(user?._id.toString());
+        if (onlineUser.size !== 0 && user && user._id) onlineUser.delete(user?._id.toString());
 
         io.emit('onlineUser', Array.from(onlineUser));
     });
